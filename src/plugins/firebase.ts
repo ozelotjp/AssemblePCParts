38fa7f84a9b2bf557149e14e65980bc5ea1f41ed
ozelotjp <contact@ozelot.app>
2020-05-10 18:20:10 +0000

    chore: firebaseのAPIKeyを追加

--- src/plugins/firebase.ts
+++ src/plugins/firebase.ts
@@ -5,7 +5,13 @@ import 'firebase/firestore'
 import 'firebase/functions'
 
 const firebaseConfig = {
-  //
+  apiKey: 'AIzaSyAtB7OIXmE3vlQP3CSDdOpDE1cua-H9pt4',
+  authDomain: 'assemblepcparts.firebaseapp.com',
+  databaseURL: 'https://assemblepcparts.firebaseio.com',
+  projectId: 'assemblepcparts',
+  storageBucket: 'assemblepcparts.appspot.com',
+  messagingSenderId: '69276848861',
+  appId: '1:69276848861:web:98b819d55624fc9b9bc134'
 }
 firebase.initializeApp(firebaseConfig)
 
