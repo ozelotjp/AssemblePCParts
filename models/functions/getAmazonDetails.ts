f6246e47a72fa7af26fef893ddb52be168582e5d
ozelotjp <contact@ozelot.app>
2020-05-11 02:13:53 +0000

    chore: getAmazonDetails関数の返り値用の型を追加

--- models/functions/getAmazonDetails.ts
+++ models/functions/getAmazonDetails.ts
@@ -1,3 +1,13 @@
 export interface IGetAmazonDetailsFunc {
   items: string[]
 }
+
+export interface IGetAmazonDetails {
+  data: Result[]
+}
+
+interface Result {
+  id: string
+  title: string
+  price: number
+}
